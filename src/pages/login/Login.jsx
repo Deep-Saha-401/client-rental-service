1badab2e527744815adec28ce20dbacb5da5c32d
Deep Saha <84536037+Deep-Saha-401@users.noreply.github.com>
2023-05-14 20:28:35 +0530

    Update Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,16 +1,14 @@
 import axios from "axios";
-
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import "./login.css";
-
 const Login = () => {
   const [credentials, setCredentials] = useState({
     username: undefined,
     password: undefined,
   });
-
+  const path ='https://api-rental-service-deep-saha-401.onrender.com/api/auth/login'
   const { loading, error, dispatch } = useContext(AuthContext);
 
   const navigate = useNavigate()
@@ -22,8 +20,9 @@ const Login = () => {
   const handleClick = async (e) => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
+  
     try {
-      const res = await axios.post("/auth/login", credentials);
+      const res = await axios.post(path, credentials);
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
       navigate("/")
     } catch (err) {
