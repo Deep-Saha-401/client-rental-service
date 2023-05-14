f46ebe640e63c9a2fbf639aa54b1c332095dd46e
Deep Saha <84536037+Deep-Saha-401@users.noreply.github.com>
2023-05-14 20:27:40 +0530

    Update Hotel.jsx

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,176 +1,142 @@
-import "./hotel.css";
-import Navbar from "../../components/navbar/Navbar";
-import Header from "../../components/header/Header";
-import MailList from "../../components/mailList/MailList";
-import Footer from "../../components/footer/Footer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCircleArrowLeft,
-  faCircleArrowRight,
-  faCircleXmark,
-  faLocationDot,
-} from "@fortawesome/free-solid-svg-icons";
-import { useContext, useState } from "react";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+
+import "./reserve.css";
 import useFetch from "../../hooks/useFetch";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
 import { SearchContext } from "../../context/SearchContext";
-import { AuthContext } from "../../context/AuthContext";
-import Reserve from "../../components/reserve/Reserve";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Monthly from "../../pages/monthly/monthly";
+import { BASE_URL } from '../../helper';
 
-
-const Hotel = () => {
-  const location = useLocation();
-  const id = location.pathname.split("/")[2];
-  const [slideNumber, setSlideNumber] = useState(0);
-  const [open, setOpen] = useState(false);
+const Reserve = ({ setOpen, hotelId }) => {
   const [openModal, setOpenModal] = useState(false);
+  const [selectedRooms, setSelectedRooms] = useState([]);
+  const { data, loading, error } = useFetch(`${BASE_URL}/hotels/room/${hotelId}`);
+  
+  const { dates } = useContext(SearchContext);
 
-  const { data, loading, error } = useFetch(`/hotels/find/${id}`);
-  const { user } = useContext(AuthContext);
+  const getDatesInRange = (startDate, endDate) => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+
+    const date = new Date(start.getTime());
+
+    const dates = [];
+
+    while (date <= end) {
+      dates.push(new Date(date).getTime());
+      date.setDate(date.getDate() + 1);
+    }
+
+    return dates;
+  };
+
+  const alldates = getDatesInRange(dates[0].startDate, dates[0].endDate);
+
+  const isAvailable = (roomNumber) => {
+    const isFound = roomNumber.unavailableDates.some((date) =>
+      alldates.includes(new Date(date).getTime())
+    );
+
+    return !isFound;
+  };
+
+  const handleSelect = (e) => {
+    const checked = e.target.checked;
+    const value = e.target.value;
+    setSelectedRooms(
+      checked
+        ? [...selectedRooms, value]
+        : selectedRooms.filter((item) => item !== value)
+    );
+  };
+
   const navigate = useNavigate();
 
-  const { dates, options } = useContext(SearchContext);
-
-  const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
-  function dayDifference(date1, date2) {
-    const timeDiff = Math.abs(date2.getTime() - date1.getTime());
-    const diffDays = Math.ceil(timeDiff / MILLISECONDS_PER_DAY);
-    return diffDays;
-  }
-
-  const days = dayDifference(dates[0].endDate, dates[0].startDate);
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  function monthDifference(date1, date2) {
-    const timeDiff = Math.abs(date2.getTime() - date1.getTime());
-    const diffDays = Math.ceil(timeDiff / MILLISECONDS_PER_DAY);
-    const diffmonth = Math.floor(diffDays/30.42)
-    return diffmonth+1;
-  
-  }
-
-  const months = monthDifference(dates[0].endDate, dates[0].startDate);
-
-  const price = () =>  {
-    const day=data.perdayrent
-    const month= data.permonthrent
-    return { day,month };
-  }
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  const handleOpen = (i) => {
-    setSlideNumber(i);
-    setOpen(true);
+  const handleClick = async () => {
+    try {
+      await Promise.all(
+        selectedRooms.map((roomId) => {
+          const res = axios.put(`${BASE_URL}/rooms/availability/${roomId}`, {
+            dates: alldates,
+          });
+          return res.data;
+        })
+      );
+      setOpen(false);
+      navigate("/");
+    } catch (err) {}
   };
-
-  const handleMove = (direction) => {
-    let newSlideNumber;
-
-    if (direction === "l") {
-      newSlideNumber = slideNumber === 0 ? 5 : slideNumber - 1;
-    } else {
-      newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1;
-    }
-
-    setSlideNumber(newSlideNumber);
-  };
-
-  const handleClick = () => {
-    if (user) {
-      setOpenModal(true);
-    } else {
-      navigate("/login");
-    }
+  const handleClickM = async () => {
+    navigate("/monthly");
   };
   return (
-    <div>
-      <Navbar />
-      <Header type="list" />
-      {loading ? (
-        "loading"
-      ) : (
-        <div className="hotelContainer">
-          {open && (
-            <div className="slider">
-              <FontAwesomeIcon
-                icon={faCircleXmark}
-                className="close"
-                onClick={() => setOpen(false)}
-              />
-              <FontAwesomeIcon
-                icon={faCircleArrowLeft}
-                className="arrow"
-                onClick={() => handleMove("l")}
-              />
-              <div className="sliderWrapper">
-                <img
-                  src={data.photos[slideNumber]}
-                  alt=""
-                  className="sliderImg"
-                />
+    <div className="reserve">
+      <div className="rContainer">
+        <FontAwesomeIcon
+          icon={faCircleXmark}
+          className="rClose"
+          onClick={() => setOpen(false)}
+        />
+        <span>Select your Plane:</span>
+        {data.map((item) => (
+          <div className="rItem" key={item._id}>
+            <div className="selectroom">
+            <div className="rItemInfo">
+              <div className="rTitle">{item.desc}</div>
+              <div className="rMax">
+                Max people: <b>{item.maxPeople}</b>
               </div>
-              <FontAwesomeIcon
-                icon={faCircleArrowRight}
-                className="arrow"
-                onClick={() => handleMove("r")}
-              />
             </div>
-          )}
-          <div className="hotelWrapper">
-            <button className="bookNow">Reserve or Book Now!</button>
-            <h1 className="hotelTitle">{data.name}</h1>
-            <div className="hotelAddress">
-              <FontAwesomeIcon icon={faLocationDot} />
-              <span>{data.address}</span>
-            </div>
-            <span className="hotelDistance">
-              Excellent location at {data.area}
-            </span>
-            <span className="hotelPriceHighlight">
-              Book a stay over Rs.{data.perdayrent} at this property
-            </span>
-            <div className="hotelImages">
-              {data.photos?.map((photo, i) => (
-                <div className="hotelImgWrapper" key={i}>
-                  <img
-                    onClick={() => handleOpen(i)}
-                    src={photo}
-                    alt=""
-                    className="hotelImg"
-                  />
-                </div>
+            <div className="rSelectRooms">
+              {item.roomNumbers.map((roomNumber) => (
+                <div className="room">
+                  <label><b>Check It:</b></label>
+                  <input type="checkbox" value={roomNumber._id} onChange={handleSelect} disabled={!isAvailable(roomNumber)} className="checkbox" />
+                </div>              
               ))}
             </div>
-            <div className="hotelDetails">
-              <div className="hotelDetailsTexts">
-                <h1 className="hotelTitle">{data.title}</h1>
-                <p className="hotelDesc">{data.description}</p>
-              </div>
-              {/* ```````````````````````````````````````````````` */}
-              <div className="hotelDetailsPrice">
-                <h1>Perfect for a {days}-night stay!</h1>
-                <span>
-                  Located in the real heart of {data.city}, this property has an
-                  excellent location score of 9.8!
-                </span>
-                <h1>
-                  <b>Rs.{data.perdayrent}/Nights :<br/> Rs.{days * data.perdayrent * options.room}</b> ({days}{" "}
-                  nights)
-                </h1>
-                <h1>
-                  <b>Rs.{data.permonthrent}/Months :<br/> Rs.{months * data.permonthrent * options.room}</b> ({months}{" "}
-                  Months)
-                </h1>
-                <button onClick={handleClick}>Reserve or Book Now!</button>
-              </div>
+            </div>
+
+          
+{/* ```````````````````````````````````````````` */}
+
+<div className="plan">
+        <div className="x">
+        <button onClick={handleClickM} className="MonthlyButton">
+                <h1>Monthly</h1>
+                <h1>Rs.{item.permonthrent}</h1>
+            </button>
+        </div>
+        <div className="x">
+            <div className=" selectroom">
+            <div className="textS">
+                <h1>Per Night</h1>
+                <h1>Rs.{item.perdayrent}</h1>
+                </div>
+                <input
+                    type="checkbox"
+
+                    // onClick={handleClick} 
+
+                    className="checkbox"
+                  />
             </div>
           </div>
-          <MailList />
-          <Footer />
+    
         </div>
-      )}
-      {openModal && <Reserve setOpen={setOpenModal} hotelId={id}/>}
+
+        </div>
+        ))}
+        <button onClick={handleClick} className="rButton">
+          Reserve Now!
+        </button>
+      </div>
+      {openModal && <Monthly setOpen={setOpenModal} hotelId={hotelId}/>}
     </div>
   );
 };
 
-export default Hotel;
+export default Reserve;
